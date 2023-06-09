735833bcdcadf6f043aa497b3a67352f9d3ba92e
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-06-09 17:51:47 +0200

    Update USBCopier.py

--- USBCopier.py
+++ USBCopier.py
@@ -1,27 +1,34 @@
+# NECESSARYYYY MODULESS FOR USEEE :DD
 import os
 import shutil
+from time import sleep
 
-# Destination folder on the Desktop
-output_folder = os.path.expanduser("E:\USBs-Output")
+# PYTHON - USB COPIER Version : 1.0.0
+# DATE 9.6.2023 SLOVAKIA
 
-# Create the output folder if it doesn't exist
-os.makedirs(output_folder, exist_ok=True)
+while True:
+    # Destination folder on the Desktop PLEASE CHANGE it !
+    output_folder = os.path.expanduser("~/Desktop/USBs-Output")
 
-# Get a list of connected USB devices
-usb_devices = [drive for drive in os.listdir("/media") if os.path.isdir(os.path.join("/media", drive))]
+    # Create the output folder if it doesn't exist
+    os.makedirs(output_folder, exist_ok=True)
 
-# Iterate over each USB device
-for usb_device in usb_devices:
-    usb_folder = os.path.join(output_folder, usb_device)
+    # Get a list of connected USB devices
+    usb_devices = [drive for drive in os.listdir("E:\\") if os.path.isdir(os.path.join("E:\\", drive))]
 
-    # Create a subfolder for the USB device
-    os.makedirs(usb_folder, exist_ok=True)
+    # Iterate over each USB device
+    for usb_device in usb_devices:
+        usb_folder = os.path.join(output_folder, usb_device)
 
-    # Copy all files from the USB device to the subfolder
-    usb_path = os.path.join("/media", usb_device)
-    for root, dirs, files in os.walk(usb_path):
-        for file in files:
-            file_path = os.path.join(root, file)
-            shutil.copy2(file_path, usb_folder)
+        # Create a subfolder for the USB device
+        os.makedirs(usb_folder, exist_ok=True)
 
-print("All files from connected USB devices have been copied to the 'USBs-Output' folder on the Desktop.")
+        # Copy all files from the USB device to the subfolder
+        usb_path = os.path.join("E:\\", usb_device)
+        for root, dirs, files in os.walk(usb_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                shutil.copy2(file_path, usb_folder)
+
+    print("All files from connected USB devices have been copied to the 'USBs-Output' folder on the Desktop.")
+    sleep(3)
