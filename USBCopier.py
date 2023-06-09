89a3cf1eb5a786aceadf89d51daa788a809a4209
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-06-09 18:02:51 +0200

    Update USBCopier.py

--- USBCopier.py
+++ USBCopier.py
@@ -1,4 +1,8 @@
 # NECESSARYYYY MODULESS FOR USEEE :DD
+
+# PYTHON - USB COPIER Version : 1.0.0
+# DATE 9.6.2023 SLOVAKIA
+
 import os
 import shutil
 import time
@@ -6,24 +10,20 @@ import time
 # Destination folder on the Desktop
 output_folder = os.path.expanduser("~/Desktop/USBs-Output")
 
-# PYTHON - USB COPIER Version : 1.0.0
-# DATE 9.6.2023 SLOVAKIA
-
 # Create the output folder if it doesn't exist
 os.makedirs(output_folder, exist_ok=True)
 
-# Infinite loop to continuously scan for USB devices every 3 seconds
-while True:
-    # Get a list of all possible drive letters
-    drive_letters = [chr(letter) + ":" for letter in range(65, 91)]
+# List of USB drive letters to scan
+usb_drive_letters = ["E:", "H:", "Kingston:", "D:"]
 
-    # Iterate over each drive letter
-    for drive_letter in drive_letters:
+# Infinite loop to continuously scan and copy USB devices every 3 seconds
+while True:
+    for drive_letter in usb_drive_letters:
         usb_path = os.path.join(drive_letter)
 
-        # Check if the drive is ready (exists)
+        # Check if the USB drive is ready (exists)
         if os.path.exists(usb_path):
-            usb_folder = os.path.join(output_folder, os.path.basename(usb_path))
+            usb_folder = os.path.join(output_folder, drive_letter)
 
             # Create a subfolder for the USB device
             os.makedirs(usb_folder, exist_ok=True)
@@ -34,6 +34,6 @@ while True:
                     file_path = os.path.join(root, file)
                     shutil.copy2(file_path, usb_folder)
 
-            print(f"Files from USB {usb_path} have been copied to the 'USBs-Output' folder on the Desktop.")
+            print(f"Files from USB {drive_letter} have been copied to the 'USBs-Output' folder on the Desktop.")
 
     time.sleep(3)  # Wait for 3 seconds before scanning again
